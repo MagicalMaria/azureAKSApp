c4c3ef1e5664be3242226a862e40557d14da2f92
MagicalMaria <mariaantonydgl@gmail.com>
2021-04-01 11:55:55 +0530

    From VS

---
+++ Models/CustomerRepo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerManagementProject.Models
+{
+    public class CustomerRepo : IRepo<Customer>
+    {
+        public List<Customer> Customers { get; set; }
+
+        public CustomerRepo()
+        {
+            Customers = new List<Customer>()
+            {
+                new Customer
+                {
+                    Id=101,
+                    Name="Ramu",
+                    Age=21
+                }
+            };
+        }
+
+        public void AddItem(Customer Item)
+        {
+            Customers.Add(Item);
+        }
+
+        public IEnumerable<Customer> GetItems()
+        {
+            return Customers;
+        }
+    }
+}
---
+++ Models/IRepo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerManagementProject.Models
+{
+    public interface IRepo<T>
+    {
+        public void AddItem(T Item);
+        public IEnumerable<T> GetItems();
+    }
+}
